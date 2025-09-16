55968dc06e1636a96fad90d114a46bf0be9861c5
Cyprian Kujur <cyprian.k2003@gmail.com>
2025-09-16 19:01:08 +0530

    fumadocs components added

--- app/components/mdxComponents.tsx
+++ app/components/mdxComponents.tsx
@@ -6,16 +6,23 @@ import { Steps, Step } from "fumadocs-ui/components/steps";
 import { Callout } from "fumadocs-ui/components/callout";
 import { CodeBlock } from "fumadocs-ui/components/codeblock";
 import { Tabs, Tab } from "fumadocs-ui/components/tabs";
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { DynamicCodeBlock } from 'fumadocs-ui/components/dynamic-codeblock';
+
 import Video from "@/components/Video";
 
 
 export const mdxComponents = {
   ...defaultMdxComponents,
+
   Steps,
   Step,
   Callout,
   CodeBlock,
   Tabs,
   Tab,
-  Video
+  Video,
+  Accordion,
+  Accordions,
+  DynamicCodeBlock
 };
